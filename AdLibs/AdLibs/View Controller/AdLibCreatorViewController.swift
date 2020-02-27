7aaa42802b736fd26dafc5a935d6e66c06af21b9
David Williams <burmesedragon@att.net>
2020-02-27 13:39:21 -0800

    Auto stash before merge of "David" and "origin/Marc"

--- AdLibs/AdLibs/View Controller/AdLibCreatorViewController.swift
+++ AdLibs/AdLibs/View Controller/AdLibCreatorViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class AdLibCreatorViewController: UIViewController {
 
+    //    MARK: - Properties and Outlets
+    
+    @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var nounTextField: UITextField!
     @IBOutlet weak var verbTextField: UITextField!
     @IBOutlet weak var pronounTextField: UITextField!
@@ -17,13 +20,51 @@ class AdLibCreatorViewController: UIViewController {
     @IBOutlet weak var adverbTextField: UITextField!
     @IBOutlet weak var colorTextView: UITextField!
     
+    var adLibController: AdLibController?
+    var themeHelper: ThemeHelper?
+    var adLib: AdLib?
+    var storyMode: Story?
     
+    var toStoryView = "SegueToStoryView"
     override func viewDidLoad() {
         super.viewDidLoad()
         
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func showStoryTapped(_ sender: UIButton) {
+        guard let title = titleTextField.text,
+            let noun = nounTextField.text,
+            let verb = verbTextField.text,
+            let pronoun = pronounTextField.text,
+            let adjective = adjectiveTextField.text,
+            let adverb = adverbTextField.text,
+            let color = colorTextView.text,
+            !title.isEmpty,
+            !noun.isEmpty,
+            !verb.isEmpty,
+            !pronoun.isEmpty,
+            !adjective.isEmpty,
+            !adverb.isEmpty,
+            !color.isEmpty else { return }
+        
+        guard let adLibController = adLibController else { return }
+            adLibController.createAdLib(title: title, noun: noun, pronoun: pronoun, verb: verb, adjective: adjective, adverb: adverb, color: color)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let adLibController = adLibController else { return }
+        if segue.identifier == toStoryView {
+            guard let destination = segue.destination as? StoryViewController else { return }
+            destination.adLibController = adLibController
+            destination.adLib = adLib
+            destination.storyMode = storyMode
+            destination.themeHelper = themeHelper
+            
+        }
+    }
+    
     
 
 
