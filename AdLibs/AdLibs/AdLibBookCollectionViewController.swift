f721180b38cb236bc0ea4b139ebb884e69ab428b
David Williams <burmesedragon@att.net>
2020-02-27 17:25:24 -0800

    added segue identifier & prepare for segue to showAdlib VC

--- AdLibs/AdLibs/AdLibBookCollectionViewController.swift
+++ AdLibs/AdLibs/AdLibBookCollectionViewController.swift
@@ -12,27 +12,28 @@ private let reuseIdentifier = "Cell"
 
 class AdLibBookCollectionViewController: UICollectionViewController {
 
+    var adLib: AdLib?
+    var adlibController = AdLibController()
+    var storyViewController = StoryViewController()
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
         // Do any additional setup after loading the view.
     }
 
-    /*
+   
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "showAdlibSegue" {
+            guard let showStoryVC = segue.destination as? StoryViewController else { return }
+            showStoryVC.adLibController = adlibController
+        }
     }
-    */
+    
 
     // MARK: UICollectionViewDataSource
 
@@ -44,7 +45,7 @@ class AdLibBookCollectionViewController: UICollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return adlibController.adLibs.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
